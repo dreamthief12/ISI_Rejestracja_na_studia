ee777b1f80bee72e8799303847540031a2cfcf9c
Amicookie <varrioue@gmail.com>
2017-03-29 22:47:05 +0200

    +f processRequest, +f registerMethod
    potrzebne do nusoap ;)

--- Rekrutacja/server.php
+++ Rekrutacja/server.php
@@ -8,6 +8,14 @@ class ServerWS {
 	public function __construct(){
 		$this->server = new nusoap_server();
 	}
+	
+	public function registerMethod($nameMethod){
+            $this->server->register($nameMethod);
+	}
+        
+	public function processRequest(){
+            $this->server->service($GLOBALS['HTTP_RAW_POST_DATA'] );
+	}
         
 }
 
