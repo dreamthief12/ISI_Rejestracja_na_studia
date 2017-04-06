ad5f3e8ac8355989a250ed9772e061366cdb5c06
annasla95 <annaslabuszewska@gmail.com>
2017-04-06 23:16:28 +0200

    login/logout

--- Rekrutacja/administracja.php
+++ Rekrutacja/administracja.php
@@ -7,8 +7,21 @@
         <?php
             require_once "./lib/nusoap.php";
             $client = new nusoap_client("http://localhost/Rekrutacja/server.php");
+            $err = $client->getError();
+            if ($err) {
+                echo '<h2>Constructor error</h2><pre>' . $err . '</pre>';
+            }
             session_start();
-            if (!isset($_SESSION['idp'])) {
+            if (isset($_GET['logout'])) {
+                session_destroy();
+                header("Location: administracja.php");
+            }
+            if((isset($_POST['log']) && isset($_POST['pas']))){
+                $result=$client->call('ServerWS.login', array('id' => $_POST['log'], 'password' => $_POST['pas'], 'who' => 'pracownik')); 
+                if($result)
+                    $_SESSION['pracownik']=1;
+                }
+            if (!isset($_SESSION['pracownik'])) {
         ?>
         <!-- stronka przed zalogowaniem -->
         <?php
