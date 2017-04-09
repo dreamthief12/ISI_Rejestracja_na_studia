8ecb1b809f85b19c28ea899b67b75bc4dff8d640
annasla95 <annaslabuszewska@gmail.com>
2017-04-09 17:33:20 +0200

    mala WIELKA zmiana

--- Rekrutacja/index.php
+++ Rekrutacja/index.php
@@ -18,7 +18,7 @@
                 header("Refresh:0; url=index.php");
             }
             if((isset($_POST['log']) && isset($_POST['pas']))){
-                $result=$client->call('ServerWS.login', array('id' => $_POST['log'], 'password' => $_POST['pas'], 'who' => 'student')); 
+                $result=$client->call('ServerWS.login', array('id' => $_POST['log'], 'password' => $_POST['pas'], 'who' => 'Student')); 
                 if($result)
                     $_SESSION['student']=1;
                 }
