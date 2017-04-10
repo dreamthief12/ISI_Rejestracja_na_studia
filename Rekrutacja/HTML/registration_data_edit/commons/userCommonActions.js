d35e7bd3e1cc5f83057145a88475eedb37de9012
Teter Tomasz <tomasz.teter@nokia.com>
2017-04-10 23:53:51 +0200

    cleanup

--- Rekrutacja/HTML/registration_data_edit/commons/userCommonActions.js
+++ Rekrutacja/HTML/registration_data_edit/commons/userCommonActions.js
@@ -7,3 +7,13 @@ function login(login, password)
 {
     alert("Logged in");
 }
+
+function fill_menu(privileges)
+{
+    var menu = document.getElementById("user_menu");
+    if(privileges == 1)
+    {
+        menu.appendChild();
+    }
+    ...
+}
